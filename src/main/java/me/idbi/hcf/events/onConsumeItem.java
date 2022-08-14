2be2d000bd76b18e7852a0de9c9b419785544709
IdbiDev <102830850+IdbiDev@users.noreply.github.com>
2022-08-14 12:25:37 +0200

    2022/08/14 Version

---
+++ src/main/java/me/idbi/hcf/events/onConsumeItem.java
@@ -0,0 +1,27 @@
+package me.idbi.hcf.events;
+
+import me.idbi.hcf.tools.HCF_Timer;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class onConsumeItem implements Listener {
+
+    @EventHandler
+    public void onConsume(PlayerItemConsumeEvent e) {
+        if(e.getItem() == null) return;
+        if(e.isCancelled()) return;
+        if(e.getItem().getData().getData() == (byte) 0 && e.getItem().getType() == Material.GOLDEN_APPLE) {
+            if(HCF_Timer.get_Golden_Apple_Time(e.getPlayer()) <= 0) {
+                HCF_Timer.add_Golden_Apple_Time(e.getPlayer());
+            } else e.setCancelled(true);
+        } else if(e.getItem().getData().getData() == (byte) 1 && e.getItem().getType() == Material.GOLDEN_APPLE) {
+            if(HCF_Timer.get_OP_Golden_Apple_Time(e.getPlayer()) <= 0) {
+                HCF_Timer.add_OP_Golden_Apple_Time(e.getPlayer());
+            } else e.setCancelled(true);
+        }
+    }
+}
