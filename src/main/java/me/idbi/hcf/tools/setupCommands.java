a4b84a2f8c5788fc41b94dc391d58a66e4f885d7
IdbiDev <102830850+IdbiDev@users.noreply.github.com>
2022-12-01 22:22:23 +0100

    Changes by Balu

--- src/main/java/me/idbi/hcf/tools/setupCommands.java
+++ src/main/java/me/idbi/hcf/tools/setupCommands.java
@@ -2,7 +2,10 @@ package me.idbi.hcf.tools;
 
 
 import me.idbi.hcf.Main;
-import me.idbi.hcf.commands.*;
+import me.idbi.hcf.commands.admin;
+import me.idbi.hcf.commands.faction;
+import me.idbi.hcf.commands.fc_position;
+import me.idbi.hcf.commands.koth;
 
 import java.util.Objects;
 
@@ -12,6 +15,11 @@ public class setupCommands {
         Objects.requireNonNull(Main.getPlugin(Main.class).getCommand("admin")).setExecutor(new admin());
         Objects.requireNonNull(Main.getPlugin(Main.class).getCommand("fc")).setExecutor(new fc_position());
         Objects.requireNonNull(Main.getPlugin(Main.class).getCommand("koth")).setExecutor(new koth());
-        Objects.requireNonNull(Main.getPlugin(Main.class).getCommand("enchantment")).setExecutor(new EnchantmentCommands());
+        //Objects.requireNonNull(Main.getPlugin(Main.class).getCommand("enchantment")).setExecutor(new EnchantmentCommands());
+        //Objects.requireNonNull(Main.getPlugin(Main.class).getCommand("addenchant")).setExecutor(new ECommand_AddEnchant());
+        //Objects.requireNonNull(Main.getPlugin(Main.class).getCommand("removeenchant")).setExecutor(new ECommand_RemoveEnchant());
+        //Objects.requireNonNull(Main.getPlugin(Main.class).getCommand("enchantbook")).setExecutor(new ECommand_EnchantBook());
+        //Objects.requireNonNull(Main.getPlugin(Main.class).getCommand("enchantinfo")).setExecutor(new ECommand_EnchantInfo());
+        //Objects.requireNonNull(Main.getPlugin(Main.class).getCommand("changelore")).setExecutor(new EnchantmentCommands());
     }
 }
